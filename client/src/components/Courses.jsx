f51695cb9be70d9691dfeda50d76ca53840ddf89
sarojkumar2003 <sarojkumarsharma2003@gmail.com>
2025-12-03 19:43:56 +0530

    code update

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Courses() {
-  const navigate = useNavigate();
-
+  const navigate = useNavigate();  
+  
   const courses = [
     {
       title: "Frontend Development",
